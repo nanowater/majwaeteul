7d2ba97c4cd9b801f17a305d044f2758dc76a8bf
nanowater <79461476+nanowater@users.noreply.github.com>
2024-01-30 02:35:49 +0900

    Create generate_TC.py

---
+++ generate_TC.py
@@ -0,0 +1,12 @@
+# 여기에 테스트케이스 제작 코드 작성
+
+"""
+예시
+
+import random
+
+def generate_test_case(index):
+    with open(f"generated_datas/INPUT{index}.txt", 'w') as file:
+        file.write(str(random.randrange(1,101)))
+
+"""
